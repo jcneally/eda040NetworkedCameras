fd6f9e87eb36e221bb9018db2013a5effe035cf7
Viktor Andersson <viktor.nossredna@gmail.com>
2012-11-09 10:03:28 +0100

    adding Receive thread

---
+++ src/Receive.java
@@ -0,0 +1,10 @@
+
+/**
+ * 
+ * @author Guttmar
+ *	Receives input stream.
+ */
+
+public class Receive extends Thread{
+
+}
---
+++ src/Recive.java
@@ -0,0 +1,10 @@
+
+/**
+ * 
+ * @author Guttmar
+ *	Recives input stream.
+ */
+
+public class Recive extends Thread{
+
+}
