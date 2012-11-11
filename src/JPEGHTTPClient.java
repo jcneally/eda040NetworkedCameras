a697d98adfa09505037884b61abb7beb7fccb771
carlosjorques <jorquesano@gmail.com>
2012-11-11 15:19:09 +0100

    README.md

--- src/JPEGHTTPClient.java
+++ src/JPEGHTTPClient.java
@@ -7,9 +7,7 @@ import java.awt.event.*;
 import java.awt.image.*;
 
 import se.lth.cs.fakecamera.*; // To gain access to maximum image size
-//Trying to synchronize
-//received
-//p
+
 public class JPEGHTTPClient {
 
     public static void main(String[] args) {
