3e8db02cf3fc3114f2faba97bb6d0c3f3e72170a
Viktor Andersson <viktor.nossredna@gmail.com>
2012-11-13 22:36:46 +0100

    update on CaptureAndsend and added class for testing

--- src/server/CaptureAndSend.java
+++ src/server/CaptureAndSend.java
@@ -20,17 +20,75 @@ public class CaptureAndSend extends Thread{
 	private byte [] jpeg = new byte[Axis211A.IMAGE_BUFFER_SIZE];
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
+	private InputStream is;
+	private OutputStream os;
+	private int port;
 	
-	public CaptureAndSend(int port, String host) throws IOException {
+	/**
+	 * Constructor, connects to the camera and socket. Waits for client to connect.
+	 * @param port
+	 * @param host //Needed and if needed where????
+	 * @throws IOException
+	 */
+	public CaptureAndSend(int port) {
 		camera = new Axis211A();
-		camera.connect();
+		this.port = port;
+	}
+	
+	
+	public void run(){
+		while(true){
+			System.out.print("1: ");
+			printByte();
+			System.out.println(camera.getJPEG(jpeg, 0));
+			printByte();
+
+			try {
+				createConnection();
+				capture();
+				sleep(0);
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Cached exception..");
+			}
+			System.out.print("2: ");
+			printByte();
+			camera.close();
+		}
+	}
+	
+	/**
+	 * For testing...
+	 */
+	private void printByte() {
+		for(int i = 0; i < 20; i++) {
+			System.out.print(jpeg[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	private void capture() throws IOException {
+		camera.getJPEG(jpeg, 0);		
+		System.out.println("-----------");
+		printByte();
+		System.out.println("-----------");
+		os = clientSocket.getOutputStream();
+				
+        int len = camera.getJPEG(jpeg,0);
+        os.write(jpeg,0,len);        
+	}
+	
+	
+	private void createConnection() throws IOException {
+		if(!camera.connect()) {
+			System.out.println("Camera failed to connect!");
+			System.exit(1);
+		}
+		System.out.println("camera connected!");		
 		serverSocket = new ServerSocket(port);
+		System.out.println("serverSocket created");
 		clientSocket = serverSocket.accept();
+		System.out.println("Socket accepted!");	// never prints this.. why?
+		clientSocket.setTcpNoDelay(true);
 	}
-	
-	public void run() {
-		camera.getJPEG(jpeg, 0);
-	}
-	
-	
 }
