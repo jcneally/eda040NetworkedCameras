e279b1ef55e463652c58b4220d4a772701998f7e
carlosjorques <jorquesano@gmail.com>
2012-11-14 12:24:59 +0100

    modified ClientController

--- src/client/ClientController.java
+++ src/client/ClientController.java
@@ -1,4 +1,6 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,46 +11,39 @@ import java.util.*;
 import javax.swing.JButton;
 
 import se.lth.cs.fakecamera.Axis211A;
-import se.lth.cs.realtime.event.Buffer;
-//test
-//test again..
-public class ClientController extends Thread {
 
-	Buffer Buffer1;
-	Buffer Buffer2;
+public class ClientController extends Thread {
 	
 	String server;
     int port;
     byte [] jpeg = new byte[Axis211A.IMAGE_BUFFER_SIZE];
         
-	public ClientController(Buffer Buffer1, Buffer Buffer2,String server,int port){
+	public ClientController(BufferedReader Buffer1, BufferedReader Buffer2,String server,int port){
 	    this.server = server;
 	    this.port = port;
-		this.Buffer1=Buffer1;
-		this.Buffer2=Buffer2;
 	}
 	
-	public void receiveData(){
+	public void receiveData(Buffer buffer){
 		try {
 	        // Open a socket to the server, get the input/output streams
 	        Socket sock = new Socket(server, port);
 	        InputStream is = sock.getInputStream();
 	        OutputStream os = sock.getOutputStream();
-	        DataInputStream istream = new DataInputStream(is);
-	
-	        // Send a simple request, always for "/image.jpg"
-	        putLine(os, "GET /image.jpg HTTP/1.0");
-	        putLine(os, "");        // The request ends with an empty line
-	
-	        // Read the first line of the response (status line)
-	        String responseLine;
-	        responseLine = getLine(is);
-	        System.out.println("HTTP server says '" + responseLine + "'.");
-	        // Ignore the following header lines up to the final empty one.
-	        do {
-	            responseLine = getLine(is);
-	        } while (!(responseLine.equals("")));
-	
+	        DataInputStream istream = new DataInputStream(is);    
+	      
+////	        // Send a simple request, always for "/image.jpg"
+////	        putLine(os, "GET /image.jpg HTTP/1.0");
+////	        putLine(os, "");        // The request ends with an empty line
+////	
+//	        // Read the first line of the response (status line)
+//	        String responseLine;
+//	        responseLine = is.read();
+//	        System.out.println("HTTP server says '" + responseLine + "'.");
+//	        // Ignore the following header lines up to the final empty one.
+//	        do {
+//	            responseLine = getLine(is);
+//	        } while (!(responseLine.equals("")));
+//	
 	        // Now load the JPEG image.
 	        int bufferSize = jpeg.length;
 	        int bytesRead  = 0;
@@ -66,23 +61,19 @@ public class ClientController extends Thread {
 	            if (status > 0) {
 	                bytesRead += status;
 	                bytesLeft -= status;
-	                istream.read();
 	            }
 	        } while (status >= 0);
 	        sock.close();
 	
+	        buffer.tryPost(jpeg);
+	        
 	        System.out.println("Received image data ("
 	                           + bytesRead + " bytes).");
-	
 	    }
 	    catch (IOException e) {
 	        System.out.println("Error when receiving image.");
 	        return;
 	    }
 	}
-	
-	private void saveImage(Buffer buffer, int currentBufferIndex){}
-	// Save the image from the socket to the buffer;
 
-	
 }
