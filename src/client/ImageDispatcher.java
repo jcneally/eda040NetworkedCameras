5bdd6d887d70ed9660aeec9e1cf2852e9923d5cf
carlosjorques <jorquesano@gmail.com>
2012-11-28 12:51:33 +0100

    Finally FPS and Delay

--- src/client/ImageDispatcher.java
+++ src/client/ImageDispatcher.java
@@ -16,18 +16,20 @@ public class ImageDispatcher extends Thread{
 	JPEGBuffer bufferCamera2;
 	GUI gui;
 	
-	private long delayCamera1;
-	private long delayCamera2;
+	private int delayCamera1;
+	private int delayCamera2;
 	
 	private final int delayUpBound = 200; //Change this delay bound to switch to async, when test the program
 	private final int delayLowBound = 100; //Change this delay bound to switch to sync, when test the program
-	private final int delayMargin = 5; //Change this delay margin to adjust the acceptable difference delay while synchronized
+	//private final int delayThreshold = 5; //Change this delay margin to adjust the acceptable difference delay while synchronized
 	private final int SYNC = 1;
 	private final int ASYNC  = 2;
 	private final int AUTO = 0;
 	private final int CAMERA1 = 1;
-	private final int CAMERA2 = 2; 
+	private final int CAMERA2 = 2;
 	
+	long startTime;
+	long updateTime;
 	int mode = ASYNC;
 	
 	public ImageDispatcher(JPEGBuffer buffer1, JPEGBuffer buffer2, GUI gui){
@@ -49,24 +51,14 @@ public class ImageDispatcher extends Thread{
 
 		case SYNC:
 			//return the most delayed image and the other with an added delay equal to the difference between both images delays
+			int delayDifference = Math.abs(delayCamera1-delayCamera2);
 			
-			if((delayCamera1-delayCamera2)>delayMargin){
-				gui.updateCamera2(bufferCamera2.getJPEG());
-				System.out.println("delay1>delay2");
-			}else if((delayCamera2-delayCamera1)>delayMargin){
-			    System.out.println("in second if");
-				gui.updateCamera1(bufferCamera1.getJPEG());	
-				System.out.println("delay2>delay1");
-			}
-            else{
+			if((System.currentTimeMillis()-updateTime)>=delayDifference){			
 				gui.updateCamera1(bufferCamera1.getJPEG());
 				gui.updateCamera2(bufferCamera2.getJPEG());	
+				updateTime = System.currentTimeMillis();
 			}
 			
-			
-//			syncImages();
-			gui.updateCamera1(bufferCamera1.getJPEG());
-			gui.updateCamera2(bufferCamera2.getJPEG());		
 			break;
 		
 		case ASYNC:
@@ -77,41 +69,38 @@ public class ImageDispatcher extends Thread{
 		}
 	}
 	
-//	private void syncImages(){
-//		//If the buffer allocate too many images, jump to the image according to the current time, and skip the old ones.
-//		//Test it with the delay of the last image
-//		if(delayCamera1>delayCamera2){
-//			if(delayCamera1<(delayCamera2+delayMargin)){
-//				bufferCamera1.skipJPEG();
-//				//delayCamera1 = bufferCamera1.getCurrentDelay();
-//			}
-//		}else if(delayCamera1<delayCamera2){
-//			if((delayCamera1+delayMargin)>delayCamera2){
-//				bufferCamera2.skipJPEG();
-//				//delayCamera2 = bufferCamera2.getCurrentDelay();
-//			}
-//		}
-//	}
-	
 	private int controlSynchronization(int currentMode){
 		//If the delay is bigger than a bound, switch to asynchronous and viceversa
 		//Do it with hysteresis to avoid switching continuously
 		int SyncMode=currentMode;
 		
-		if(delayCamera1>delayUpBound||delayCamera2>delayUpBound){
+		if(Math.abs(delayCamera1-delayCamera2)>delayUpBound){
 			SyncMode = ASYNC;
-		}else if(delayCamera1<delayLowBound&&delayCamera2<delayLowBound){
+		}else if(Math.abs(delayCamera1-delayCamera2)<delayLowBound){
 			SyncMode = SYNC;
 		}
 		return SyncMode;
 	}
 	
+	
+	private void refreshData(){
+		if((System.currentTimeMillis()-startTime)>=1000){
+			gui.setFPS(bufferCamera1.getNumOfImg(), CAMERA1);
+			gui.setFPS(bufferCamera2.getNumOfImg(), CAMERA2);
+			startTime = System.currentTimeMillis();
+		}
+		
+		delayCamera1 = bufferCamera1.getDelay();
+		delayCamera2 = bufferCamera2.getDelay();
+		gui.setDelay(delayCamera1, CAMERA1);
+		gui.setDelay(delayCamera2, CAMERA2);
+	}
+	
 	public void run(){
 		while(true){
+		refreshData();
+		refreshImage(ASYNC);
 		System.out.println("Delay 1: "+delayCamera1+" Delay 2: "+delayCamera2);
-		delayCamera1 = bufferCamera1.getCurrentDelay();
-		delayCamera2 = bufferCamera2.getCurrentDelay();
-		refreshImage(SYNC);
 		}
 	}
 }
--- src/client/JPEGBuffer.java
+++ src/client/JPEGBuffer.java
@@ -12,24 +12,31 @@ class JPEGBuffer {
 	///////////////////////////////////////////////////////////////////////////////////
 	
 	int available = 0;		// Number of lines that are available. 
-	final int size=10;		// The max number of buffered lines.
+	final int size=24;		// The max number of buffered lines.
 	final int jpegsize = Axis211A.IMAGE_BUFFER_SIZE;
 	byte[][] buffData;	// The actual buffer.
+	int [] buffDelay;
 	int nextToPut = 0;		// Writers index.
 	int nextToGet = 0;		// Readers index.
 	long offset = 0l;
+	int images = 0;
+
 
 	public JPEGBuffer() {
 		buffData = new byte[size][jpegsize];
+		buffDelay = new int[size];
 	}
 
-	synchronized void putJPEG(byte[] inp) {
+	synchronized void putJPEG(byte[] data) {
 		try {
 			while (available==size) wait();
 		} catch (InterruptedException exc) {
 			throw new RTError("Buffer.putJPEG interrupted: "+exc);
 		};
 		buffData[nextToPut] = inp;
+		long time = 1000L*(((data[25]<0?256+data[25]:data[25])<<24)+((data[26]<0?256+data[26]:data[26])<<16)+((data[27]<0?256+data[27]:data[27])<<8)+(data[28]<0?256+data[28]:data[28]))+10L*(data[29]<0?256+data[29]:data[29]);
+		buffDelay[nextToPut]= (int) (System.currentTimeMillis()-time);
+		buffData[nextToPut] = data;
 		if (++nextToPut == size) nextToPut = 0;
 		available++;
 		notifyAll(); // Only notify() could wake up another producer.
@@ -43,36 +50,32 @@ class JPEGBuffer {
 		}
 		byte [] ans = buffData[nextToGet];
 		buffData[nextToGet] = null;
+		buffDelay[nextToGet] = -1;
 		if(++nextToGet == size) nextToGet = 0;
 		available--;
+		images++;
 		notifyAll();
 		return ans;
 	}
 	
-	synchronized long getCurrentDelay(){
+
+	synchronized int getNumOfImg(){
+		int ans = images;
+		images = 0;
+		return ans;
+	}
+	
+	synchronized int getDelay(){
 		//return the delay of the current image of 'camera'
 		
 		try {
 			while(available == 0) wait();
 		} catch (InterruptedException e) {
-			throw new RTError("Buffer.getCurrentDelay interrupted:" + e);
+			throw new RTError("Buffer.getDelay interrupted:" + e);
 		}
-		byte[] data = buffData[nextToGet];
+		
 		notifyAll();
-		long time = 1000L*(((data[25]<0?256+data[25]:data[25])<<24)+((data[26]<0?256+data[26]:data[26])<<16)+((data[27]<0?256+data[27]:data[27])<<8)+(data[28]<0?256+data[28]:data[28]))+10L*(data[29]<0?256+data[29]:data[29]);
-		return  (System.currentTimeMillis()-time-offset);
+		return (buffDelay[nextToGet]);
 	}
-	
-	synchronized void skipJPEG(){
-		//Erase unused images due to its high delay and jumps to the next read position
-		try {
-			while(available == 0) wait();
-		} catch (InterruptedException e) {
-			throw new RTError("Buffer.skipJPEG interrupted:" + e);
-		}
-		buffData[nextToGet] = null;
-		if(++nextToGet >= size) nextToGet = 0;
-		available--;
-		notifyAll();
-	}
+
 }
